5b7745e5a0120f2f439e833220b4408f91da429e
jakubpw <jawalendowski@gmail.com>
2020-05-22 22:16:35 +0200

    get_meme

--- routes/meme.js
+++ routes/meme.js
@@ -1,41 +1,30 @@
 var express = require('express');
 var router = express.Router();
-
+function get_meme(db, id, res) {
+  db.get("SELECT memes.id, name, price, url FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem WHERE date = (SELECT MAX(date) FROM prices WHERE memes.id = idmem)",
+  id,
+  function (err, row) {
+    db.all("SELECT date, price FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem ORDER BY date DESC",
+      id,
+      function (err, rows) {
+        res.render('meme',
+          {
+            title: 'Meme market',
+            meme: row,
+            prices: rows
+          });
+      })
+  })
+}
 /* GET users listing. */
 router.get('/:memeId', function (req, res) {
-  req.db.get("SELECT memes.id, name, price, url FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem WHERE date = (SELECT MAX(date) FROM prices WHERE memes.id = idmem)",
-    req.params.memeId,
-    function (err, row) {
-      req.db.all("SELECT date, price FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem ORDER BY date DESC",
-        req.params.memeId,
-        function (err, rows) {
-          res.render('meme',
-            {
-              title: 'Meme market',
-              meme: row,
-              prices: rows
-            });
-        })
-    })
-
+  get_meme(req.db, req.params.memeId, res)
 })
 router.post('/:memeId', function (req, res) {
+  // Dodajemy do tabeli cen nowa cene mema z aktualna data (change_price).
   req.db.run("INSERT INTO prices (idmem, price, date) VALUES (?, ?, ?)", req.params.memeId, req.body.price, new Date().toISOString().slice(0, 19).replace('T', ' '));
   console.log(req.body.price);
-  req.db.get("SELECT memes.id, name, price, url FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem WHERE date = (SELECT MAX(date) FROM prices WHERE memes.id = idmem)",
-    req.params.memeId,
-    function (err, row) {
-      req.db.all("SELECT date, price FROM (SELECT * FROM memes WHERE id = ?) memes INNER JOIN prices ON memes.id = idmem ORDER BY date DESC",
-        req.params.memeId,
-        function (err, rows) {
-          res.render('meme',
-            {
-              title: 'Meme market',
-              meme: row,
-              prices: rows
-            });
-        })
-    })
+  get_meme(req.db, req.params.memeId, res)
 })
 
 module.exports = router;
